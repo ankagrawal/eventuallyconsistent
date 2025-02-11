4bc9dda48286e1a85aac667f2ce551c0251e17b5
Ankur Agrawal <ankuagrawal@linkedin.com>
2025-02-11 01:29:34 -0800

    cores fix

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbw1FKjPXfETbrwQqxeeCTGl7IuYMsx3XZeN5rIcyBJ6NzF4FoPvoeeWaO_qRmJHAztc/exec'
+    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyC4QblB7_TW1rHXZazXCUvpAFKjK9ZBNy8iSC3LYyfQF4Zs5vj1QLaZWJGXn-LKmS8/exec'
 }; 
