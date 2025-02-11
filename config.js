851d4d983d1b12f694ff78fed7a82e3d4a8c06e0
Ankur Agrawal <ankuagrawal@linkedin.com>
2025-02-11 01:42:19 -0800

    Update

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyC4QblB7_TW1rHXZazXCUvpAFKjK9ZBNy8iSC3LYyfQF4Zs5vj1QLaZWJGXn-LKmS8/exec'
+    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycby614T-utwdOZg8qDJ6YOJJ2iYYOc_hcWS0zK58UMQ_G53PwixmPL4j7fjeE_Bgh6jm/exec'
 }; 
